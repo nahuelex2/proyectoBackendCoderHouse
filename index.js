06a57bce980395a7679b050a362c8d2191d6f25e
Nahuel Medina <nahuelmedina738@gmail.com>
2024-01-15 19:57:06 -0300

    working on this

--- index.js
+++ index.js
@@ -9,64 +9,64 @@ app.listen(port, () => {
 })
 
 
-main = async () => {
+// main = async () => {
 
-    // creacion de instancia de ProductManager
-    const productManager = new ProductManager('./data.json')
+//     // creacion de instancia de ProductManager
+//     const productManager = new ProductManager('./data.json')
 
-    // Primer llamada a getProducts() 
-    let Products = await productManager.getProducts()
-    console.log(Products);
-
-
-    let product = {
-        title: 'Producto de prueba',
-        description: 'Este es un producto de prueba',
-        price: 200,
-        thumbnail: 'Sin imagen',
-        code: 'bbb',
-        stock: 25
-    }
+//     // Primer llamada a getProducts() 
+//     let Products = await productManager.getProducts()
+//     console.log(Products);
 
 
-    try {
-        await productManager.addProduct(product)
-    } catch (error) {
-        console.log(error);
-    }
+//     let product = {
+//         title: 'Producto de prueba',
+//         description: 'Este es un producto de prueba',
+//         price: 200,
+//         thumbnail: 'Sin imagen',
+//         code: 'bbb',
+//         stock: 25
+//     }
 
 
-    // //segundo llamado a getproducts Esta vez trae con el producto agregado
-    // let products = await productManager.getProducts()
-    // console.log(products);
+//     try {
+//         await productManager.addProduct(product)
+//     } catch (error) {
+//         console.log(error);
+//     }
 
 
-
-    //getProductById
-    // let ProductById = await productManager.getProductById(1)
-    // console.log(ProductById);
-
-
-    //updateProduct
-    // await productManager.updateProduct(23, 'stock', 20)
-    // console.log(await productManager.getProducts());
-
-
-    //delete product
-    // await productManager.deleteProduct(1)
-    // console.log(await productManager.getProducts());
+//     // //segundo llamado a getproducts Esta vez trae con el producto agregado
+//     // let products = await productManager.getProducts()
+//     // console.log(products);
 
 
 
-}
+//     //getProductById
+//     // let ProductById = await productManager.getProductById(1)
+//     // console.log(ProductById);
 
+
+//     //updateProduct
+//     // await productManager.updateProduct(23, 'stock', 20)
+//     // console.log(await productManager.getProducts());
+
+
+//     //delete product
+//     // await productManager.deleteProduct(1)
+//     // console.log(await productManager.getProducts());
+
+
+
+// }
+const productManager = new ProductManager('./data.json')
 // main()
 app.get('/', (req, res) => {
     res.send('bienvenido al eccomerce')
 })
 
-app.get('allProducts', async (req, res) => {
+app.get('/allProducts', async (req, res) => {
     let Products = await productManager.getProducts()
     console.log(Products);
-    res.send(JSON.parse(Products))
+    res.send(Products)
 })
